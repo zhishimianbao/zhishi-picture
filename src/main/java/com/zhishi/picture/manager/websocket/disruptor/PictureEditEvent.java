fd2a1f287eb0f332f255cc6e1af2dc929f2abb34
hjx <1677518554@qq.com>
2026-02-25 15:32:43 +0800

    图片协同编辑

---
+++ src/main/java/com/zhishi/picture/manager/websocket/disruptor/PictureEditEvent.java
@@ -0,0 +1,33 @@
+package com.zhishi.picture.manager.websocket.disruptor;
+import com.zhishi.picture.manager.websocket.model.PictureEditRequestMessage;
+import com.zhishi.picture.model.entity.User;
+import lombok.Data;
+import org.springframework.web.socket.WebSocketSession;
+
+/**
+ * 图片编辑事件
+ */
+@Data
+public class PictureEditEvent {
+
+    /**
+     * 消息
+     */
+    private PictureEditRequestMessage pictureEditRequestMessage;
+
+    /**
+     * 当前用户的 session
+     */
+    private WebSocketSession session;
+
+    /**
+     * 当前用户
+     */
+    private User user;
+
+    /**
+     * 图片 id
+     */
+    private Long pictureId;
+
+}
